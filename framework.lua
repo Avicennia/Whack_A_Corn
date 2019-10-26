9acec2bff1ee360d1e7027589d68f210759efdd8
Aaron Suen <warr1024@gmail.com>
2019-10-26 10:26:52 -0400

    Easier particle definition helper.

--- framework.lua
+++ framework.lua
@@ -15,6 +15,16 @@ wac.find_nodes = function(pos, min, max, ...)
 	return minetest.find_nodes_in_area(min, max, ...)
 end
 
+wac.anynode = function(...)
+	local t = {...}
+	for _, name in ipairs(t) do
+		if minetest.registered_nodes[name] then
+			return name
+		end
+	end
+	error("No nodes found from " .. table.concat(t, ", "))
+end
+
 wac.pickrand = function (list, rarityfunc, picked)
 	local total = 0
 	local items = {}
@@ -38,78 +48,84 @@ end
 -- Just for the sake of slightly better separation of components.
 
 --	--	--	--	--	--	PARTICLES	--	--	--	--	--	--
-wac.spewparticles = function(pos,tex)
-	minetest.add_particlespawner({
-		amount = 4,
+
+local function unpackrange(t)
+	return {
+		x = t.x or t[1] or 0,
+		y = t.y or t[2] or 0,
+		z = t.z or t[3] or 0
+	}, {
+		x = t.x or t[4] or t[1] or 0,
+		y = t.y or t[5] or t[2] or 0,
+		z = t.z or t[6] or t[3] or 0
+	}
+end
+local function smarticles(def)
+	if def.posrange then
+		def.minpos, def.maxpos = unpackrange(def.posrange)
+	end
+	if def.offset then
+		local min, max = unpackrange(def.offset)
+		def.minpos = vector.add(def.minpos, min)
+		def.maxpos = vector.add(def.maxpos, max)
+	end
+	if def.velrange then
+		def.minvel, def.maxvel = unpackrange(def.velrange)
+	end
+	if def.accrange then
+		def.minacc, def.maxacc = unpackrange(def.accrange)
+	end
+	if def.exprange then
+		def.minexptime = def.exprange[1]
+		def.maxexptime = def.exprange[2] or def.exprange[1]
+	end
+	if def.sizerange then
+		def.minsize = def.sizerange[1]
+		def.maxsize = def.sizerange[2] or def.sizerange[1]
+	end
+	return minetest.add_particlespawner(def)
+end
+
+wac.dimond_focused_lazer = function(pos, tex)
+	if not pos then return end
+	smarticles({
+		amount = 29,
 		time = 1,
-		minpos = {x=pos.x-0.98, y=pos.y+0.98, z=pos.z-0.98},
-		maxpos = {x=pos.x+0.98, y=pos.y+0.98, z=pos.z+0.98},
-		minvel = {x=0.1, y=1, z=0.1},
-		maxvel = {x=0.2, y=3, z=0.2},
-		minacc = {x=0, y=0, z=0},
-		maxacc = {x=0.1, y=0.4, z=0.1},
-		minexptime = 1,
-		maxexptime = 4,
-		minsize = 1,
-		maxsize = 2,
-		collisiondetection = false,
-		collision_removal = false,
-		vertical = false,
+		offset = pos,
+		posrange = {0, -1, 0, 0, 10, 0},
+		accrange = {-0.1, 0, -0.1, 0.1, 0, 0.1},
+		exprange = {0.1, 0.5},
+		sizerange = {40},
+		vertical = true,
 		texture = tex,
 		glow = 2
 	})
-end
-wac.dimond_focused_lazer = function(pos,tex)
-	if(pos)then
-		minetest.add_particlespawner({
-			amount = 29,
-			time = 1,
-			minpos = {x=pos.x, y=pos.y+10, z=pos.z},
-			maxpos = {x=pos.x, y=pos.y-1, z=pos.z},
-			minvel = {x=0.01, y=0, z=0.01},
-			maxvel = {x=0.02, y=0, z=0.02},
-			minacc = {x=0, y=0, z=0},
-			maxacc = {x=0.1, y=0.4, z=0.1},
-			minexptime = 0.1,
-			maxexptime = 0.5,
-			minsize = 40,
-			maxsize = 40,
-			collisiondetection = false,
-			collision_removal = false,
-			vertical = true,
-			texture = tex,
-			glow = 2
-		})
-		minetest.sound_play({name ="wac_lbeam"},{
-			pos = pos,
-			gain = 50.0, -- default
-			max_hear_distance = 32,
-		})
-	end
-end
-wac.warr_ham = function(pos)
-	minetest.sound_play({name ="wac_warrhammerwave"},{
+	minetest.sound_play("wac_lbeam", {
 		pos = pos,
-		gain = 50.0, -- default
-		max_hear_distance = 32,})
+		gain = 1,
+		max_hear_distance = 32
+	})
 end
+
+wac.warr_ham = function(pos)
+	minetest.sound_play("wac_warrhammerwave", {
+		pos = pos,
+		gain = 1,
+		max_hear_distance = 32
+	})
+end
+
 wac.tumbleparticles = function(pos,tex)
-	minetest.add_particlespawner({
+	smarticles({
 		amount = 40,
 		time = 2,
-		minpos = {x=pos.x-0.8, y=pos.y, z=pos.z-0.8},
-		maxpos = {x=pos.x+0.8, y=pos.y, z=pos.z+0.8},
-		minvel = {x=0, y=0, z=0},
-		maxvel = {x=0, y=0, z=0},
-		minacc = {x=0, y=0.5, z=0},
-		maxacc = {x=0.1, y=1, z=0.1},
-		minexptime = 0.3,
-		maxexptime = 0.6,
-		minsize = 1,
-		maxsize = 5,
+		offset = pos,
+		posrange = {-0.8, 0, -0.8, 0.8, 0, 0.8},
+		accrange = {0, 0.5, 0, 0.1, 1, 0.1},
+		exprange = {0.3, 0.6},
+		sizerange = {1, 5},
 		collisiondetection = false,
 		collision_removal = false,
-		vertical = false,
 		texture = tex,
 		glow = 2
 	})
@@ -124,35 +140,16 @@ wac.scoreparticles = function(pos, score)
 		txr = txr .. ":" .. (i * 8) .. "," .. (#score * 3)
 			.. "=wac_fx_numeric.png\\^[verticalframe\\:11\\:" .. j
 	end
-	minetest.add_particlespawner({
+	smarticles({
 		amount = 25,
 		time = 0.1,
-		minpos = {x=pos.x-2, y=pos.y, z=pos.z-2},
-		maxpos = {x=pos.x+2, y=pos.y+2, z=pos.z+2},
-		minvel = {x=0, y=0, z=0},
-		maxvel = {x=0, y=0, z=0},
-		minacc = {x=0, y=0.5, z=0},
-		maxacc = {x=0.1, y=1, z=0.1},
-		minexptime = 0.3,
-		maxexptime = 0.6,
-		minsize = 2 * scale,
-		maxsize = 2 * scale,
-		collisiondetection = false,
-		collision_removal = false,
-		vertical = false,
+		offset = pos,
+		posrange = {-2, -0.5, -2, 2, 1.5, 2},
+		exprange = {0.3, 0.6},
+		sizerange = {2 * scale},
 		texture = txr,
 		glow = 2
 	})
-end
-
-wac.anynode = function(...)
-	local t = {...}
-	for _, name in ipairs(t) do
-		if minetest.registered_nodes[name] then
-			return name
-		end
-	end
-	error("No nodes found from " .. table.concat(t, ", "))
 end
 
 --	--	--	--	--	--	PARTICLES	--	--	--	--	--	--	^^
